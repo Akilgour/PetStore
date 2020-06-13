d087fb04a2dcb278c4ca510707bbe8222cb2429b
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-13 17:07:37 +0100

    Set up database

---
+++ PetStore.Data/Context/PetStoreContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetStore.Domain.Models;
+
+namespace PetStore.Data.Context
+{
+    public class PetStoreContext : DbContext
+    {
+        public PetStoreContext(DbContextOptions<PetStoreContext> options)
+            : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        public PetStoreContext()
+        {
+
+        }
+
+        public DbSet<StockItem> StockItems { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = PetStore");
+            }
+        }
+    }
+}
---
+++ PetStore.Data/ReadMe.txt
@@ -0,0 +1,11 @@
+﻿﻿
+                     _/\__
+               ---==/    \\
+         ___  ___   |.    \|\
+        | __|| __|  |  )   \\\
+        | _| | _|   \_/ |  //|\\
+        |___||_|       /   \\\/\\
+
+In package manager 
+Get Help
+		get-help entityframeworkcore
