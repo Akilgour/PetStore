91a02885fc0689a4d792dc4031447059c3d90792
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-13 16:23:16 +0100

    Added a base model

---
+++ PetStore.Domain/Models/StockItem.cs
@@ -0,0 +1,9 @@
+﻿namespace PetStore.Domain.Models
+{
+    public class StockItem : BaseModel
+    {
+        public string Name { get; set; }
+        public int WeightInKg { get; set; }
+        public int Quantity { get; set; }
+    }
+}
