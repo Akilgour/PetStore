14210afa80b6a02f0fb95b14ee5605a0e5bf753c
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-15 11:56:55 +0100

    Added basic send and get

--- PetStore.StockDelivery.Publisher/HelloWorldSend.cs
+++ PetStore.StockDelivery.Publisher/HelloWorldSend.cs
@@ -1,35 +1,61 @@
 ﻿using PetStore.Shared.Helpers;
 using PetStore.Shared.QueMessages;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System;
 
 internal class Program
 {
-    private static ConnectionFactory _factory;
-    private static IConnection _connection;
-    private static IModel _model;
-    private const string QueueName = "StandardQueue_ExampleQueue";
-
     private static void Main(string[] args)
     {
         var textMessage01 = new TextMessage { Id = 1, Text = "A1" };
         var textMessage02 = new TextMessage { Id = 2, Text = "B2" };
-        CreateQueue();
-        SendMessage(textMessage01);
-        SendMessage(textMessage02);
+
+        var factory = new ConnectionFactory() { HostName = "localhost", UserName = "guest", Password = "guest" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            channel.BasicPublish("", "hello", null, textMessage01.Serialize());
+            channel.BasicPublish("", "hello", null, textMessage02.Serialize());
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+
+        Get();
     }
 
-    private static void CreateQueue()
+    public static void Get()
     {
-        _factory = new ConnectionFactory { HostName = "localhost", UserName = "guest", Password = "guest" };
-        _connection = _factory.CreateConnection();
-        _model = _connection.CreateModel();
-        _model.QueueDeclare(QueueName, true, false, false, null);
-    }
+        var factory = new ConnectionFactory() { HostName = "localhost", UserName = "guest", Password = "guest" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
 
-    private static void SendMessage(TextMessage message)
-    {
-        _model.BasicPublish("", QueueName, null, message.Serialize());
-        Console.WriteLine("   Message Sent  ");
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message1 = (TextMessage)body.DeSerialize(typeof(TextMessage));
+                Console.WriteLine($" id {message1.Id}  Text {message1.Text}");
+            };
+            channel.BasicConsume(queue: "hello",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
     }
 }
