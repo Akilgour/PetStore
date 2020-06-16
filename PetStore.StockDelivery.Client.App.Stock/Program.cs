e44ae4e10a6648e24341885387cff6a7c00cb530
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-16 11:36:06 +0100

    Added a test client project

---
+++ PetStore.StockDelivery.Client.App.Stock/Program.cs
@@ -0,0 +1,16 @@
+﻿using PetStore.Shared;
+using PetStore.StockDelivery.Client.Client;
+using System;
+
+namespace PetStore.StockDelivery.Client.App.Stock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("StockDelivery Client");
+            var client = new StockDeliveryClient(new RabbitMQConfig("localhost", "guest", "guest"));
+            client.Receive();
+        }
+    }
+}
---
+++ PetStore.StockDelivery.Client/Client/StockDeliveryClient.cs
@@ -0,0 +1,50 @@
+﻿using PetStore.Domain.Models;
+using PetStore.Shared;
+using PetStore.Shared.Helpers;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore.StockDelivery.Client.Client
+{
+    public class StockDeliveryClient
+    {
+        public StockDeliveryClient(RabbitMQConfig rabbitMQConfig)
+        {
+        }
+
+        public  void Receive()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", UserName = "guest", Password = "guest" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "StockDelivery_Que",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var stockItem = (StockItem)body.DeSerialize(typeof(StockItem));
+                    NewMethod(stockItem);
+                };
+                channel.BasicConsume(queue: "StockDelivery_Que",
+                                     autoAck: true,
+                                     consumer: consumer);
+            }
+        }
+
+        private static void NewMethod(StockItem stockItem)
+        {
+            Console.WriteLine($" id {stockItem.Id}  Name {stockItem.Name}");
+        }
+    }
+}
