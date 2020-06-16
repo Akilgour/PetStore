3d6e6e1d90ecd784a244f6fffbe02dc0b4c01b38
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-16 12:29:06 +0100

    Added StockDeliveryManager and put a Create that calls down to the reposity Added manager project

---
+++ PetStore.StockDelivery.Manager/Managers/Interface/IStockDeliveryManager.cs
@@ -0,0 +1,10 @@
+﻿using PetStore.Domain.Models;
+using System.Threading.Tasks;
+
+namespace PetStore.StockDelivery.Manager.Managers.Interface
+{
+    public interface IStockDeliveryManager
+    {
+        Task Create(StockItem stockItem);
+    }
+}
---
+++ PetStore.StockDelivery.Manager/Managers/StockDeliveryManager.cs
@@ -0,0 +1,22 @@
+﻿using PetStore.Data.Repositorys.Interface;
+using PetStore.Domain.Models;
+using PetStore.StockDelivery.Manager.Managers.Interface;
+using System.Threading.Tasks;
+
+namespace PetStore.StockDelivery.Manager.Managers
+{
+    public class StockDeliveryManager : IStockDeliveryManager
+    {
+        private readonly IStockItemRepository _stockItemRepository;
+
+        public StockDeliveryManager(IStockItemRepository stockItemRepository)
+        {
+            _stockItemRepository = stockItemRepository;
+        }
+
+        public async Task Create(StockItem stockItem)
+        {
+            await _stockItemRepository.Create(stockItem);
+        }
+    }
+}
