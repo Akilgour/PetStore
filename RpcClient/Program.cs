f063535c22efd05b99802c4587054f2ccd281eba
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-17 11:34:50 +0100

    Added a RPC demo project

---
+++ RpcClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RpcClient
+{
+    public class Rpc
+    {
+        private static async Task Main(string[] args)
+        {
+            Console.Title = "RabbitMQ RPC Client";
+            //I stole this from https://gigi.nullneuron.net/gigilabs/abstracting-rabbitmq-rpc-with-taskcompletionsource/
+
+            using (var rpcClient = new RpcClient())
+            {
+                Console.WriteLine("Press ENTER or Ctrl+C to exit.");
+
+                while (true)
+                {
+                    string message = null;
+
+                    Console.Write("Enter a message to send: ");
+                    using (var colour = new ScopedConsoleColour(ConsoleColor.Blue))
+                    {
+                        message = Console.ReadLine();
+                    }
+
+                    if (string.IsNullOrWhiteSpace(message))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        var response = await rpcClient.SendAsync(message);
+
+                        Console.Write("Response was: ");
+                        using (var colour = new ScopedConsoleColour(ConsoleColor.Green))
+                        {
+                            Console.WriteLine(response);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ RpcClient/ScopedConsoleColour.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RpcClient
+{
+    public class ScopedConsoleColour : IDisposable
+    {
+        private ConsoleColor oldColour;
+
+        public ScopedConsoleColour(ConsoleColor newColour)
+        {
+            this.oldColour = Console.ForegroundColor;
+            Console.ForegroundColor = newColour;
+        }
+
+        public void Dispose()
+        {
+            Console.ForegroundColor = this.oldColour;
+        }
+    }
+}
