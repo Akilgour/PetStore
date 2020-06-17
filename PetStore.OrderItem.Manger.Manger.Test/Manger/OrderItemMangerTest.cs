9c44a1cd19cfd5298432576b2b237423ce3f3318
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-17 17:25:13 +0100

    Stubbed out OrderItemManger and added tests

---
+++ PetStore.OrderItem.Manger.Manger.Test/Manger/OrderItemMangerTest.cs
@@ -0,0 +1,106 @@
+﻿using Moq;
+using PetStore.Data.Repositorys.Interface;
+using PetStore.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PetStore.OrderItem.Manger.Manger.Test.Manger
+{
+    public class OrderItemMangerTest
+    {
+        [Fact]
+        public async Task OrderOkay()
+        {
+            //arrange
+            var repository = new Mock<IStockItemRepository>();
+            repository.Setup(x => x.GetByName("AA")).Returns(Task.FromResult(new StockItem() { Name = "AA", Quantity = 10 }));
+            repository.Setup(x => x.GetByName("BB")).Returns(Task.FromResult(new StockItem() { Name = "BB", Quantity = 10 }));
+            repository.Setup(x => x.GetByName("CC")).Returns(Task.FromResult(new StockItem() { Name = "CC", Quantity = 10 }));
+
+            var orderItemManger = new OrderItemManger(repository.Object);
+
+            var stockOrder = new StockOrder()
+            {
+                OrderNumber = "ABC 123",
+                OrderItems = new List<StockItem>()
+                {
+                    new StockItem() { Name = "AA", Quantity = 2 },
+                    new StockItem() { Name = "BB", Quantity = 3 },
+                    new StockItem() { Name = "CC", Quantity = 4 }
+                }
+            };
+
+            //act
+            var value = await orderItemManger.Order(stockOrder);
+
+            //assert
+            Assert.Equal("asdf", value.Message);
+            Assert.True(value.Success);
+
+            repository.Verify(a => a.Update(It.Is<StockItem>(s => s.Name == "AA" && s.Quantity == 8)), Times.Once);
+            repository.Verify(a => a.Update(It.Is<StockItem>(s => s.Name == "BB" && s.Quantity == 7)), Times.Once);
+            repository.Verify(a => a.Update(It.Is<StockItem>(s => s.Name == "CC" && s.Quantity == 6)), Times.Once);
+        }
+
+        [Fact]
+        public async Task Order_TwoItems_NotEnoughtStock()
+        {
+            //arrange
+            var repository = new Mock<IStockItemRepository>();
+            repository.Setup(x => x.GetByName("AA")).Returns(Task.FromResult(new StockItem() { Name = "AA", Quantity = 10 }));
+            repository.Setup(x => x.GetByName("BB")).Returns(Task.FromResult(new StockItem() { Name = "BB", Quantity = 10 }));
+            repository.Setup(x => x.GetByName("CC")).Returns(Task.FromResult(new StockItem() { Name = "CC", Quantity = 10 }));
+
+            var orderItemManger = new OrderItemManger(repository.Object);
+
+            var stockOrder = new StockOrder()
+            {
+                OrderNumber = "ABC 123",
+                OrderItems = new List<StockItem>()
+                {
+                    new StockItem() { Name = "AA", Quantity = 20 },
+                    new StockItem() { Name = "BB", Quantity = 3 },
+                    new StockItem() { Name = "CC", Quantity = 40 }
+                }
+            };
+
+            //act
+            var value = await orderItemManger.Order(stockOrder);
+
+            //assert
+            Assert.Equal("Order ABC 123, can not be placed not enought stock AA, CC.", value.Message);
+            Assert.False(value.Success);
+        }
+
+        [Fact]
+        public async Task Order_OneItem_NotEnoughtStock()
+        {
+            //arrange
+            var repository = new Mock<IStockItemRepository>();
+            repository.Setup(x => x.GetByName("AA")).Returns(Task.FromResult(new StockItem() { Name = "AA", Quantity = 10 }));
+            repository.Setup(x => x.GetByName("BB")).Returns(Task.FromResult(new StockItem() { Name = "BB", Quantity = 10 }));
+            repository.Setup(x => x.GetByName("CC")).Returns(Task.FromResult(new StockItem() { Name = "CC", Quantity = 10 }));
+
+            var orderItemManger = new OrderItemManger(repository.Object);
+
+            var stockOrder = new StockOrder()
+            {
+                OrderNumber = "ABC 345",
+                OrderItems = new List<StockItem>()
+                {
+                    new StockItem() { Name = "AA", Quantity = 2 },
+                    new StockItem() { Name = "BB", Quantity = 30 },
+                    new StockItem() { Name = "CC", Quantity = 4 }
+                }
+            };
+
+            //act
+            var value = await orderItemManger.Order(stockOrder);
+
+            //assert
+            Assert.Equal("Order ABC 345, can not be placed not enought stock BB.", value.Message);
+            Assert.False(value.Success);
+        }
+    }
+}
---
+++ PetStore.OrderItem.Manger/Manger/OrderItemManger.cs
@@ -0,0 +1,26 @@
+﻿using PetStore.Data.Repositorys.Interface;
+using PetStore.Domain.Models;
+using PetStore.Shared.QueMessages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore.OrderItem.Manger.Manger
+{
+    public class OrderItemManger
+    {
+        private readonly IStockItemRepository _stockItemRepository;
+
+        public OrderItemManger(IStockItemRepository  stockItemRepository)
+        {
+            _stockItemRepository = stockItemRepository;
+        }
+
+        public async Task<OrderResponse> Order(StockOrder stockOrder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
