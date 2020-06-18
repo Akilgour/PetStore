dffffea8a6d1a2dc9d8e1292dd29a609a20ea1cb
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-18 11:38:43 +0100

    Added and set up autofac on the project

---
+++ PetStore.OrderItem.Manger/Autofac/OrderItemManagerModule.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using PetStore.Data.Autofac;
+using System.Linq;
+
+namespace PetStore.OrderItem.Manger.Autofac
+{
+    public class OrderItemManagerModule : Module
+    {
+        public OrderItemManagerModule()
+        {
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new PetStoreDataModule());
+
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                .Where(t => t.Name.EndsWith("Manager"))
+                .AsImplementedInterfaces();
+        }
+    }
+}
---
+++ PetStore.OrderItem.Server/Autofac/AutofacConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using PetStore.OrderItem.Manger.Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore.OrderItem.Server.Autofac
+{
+    public class AutofacConfiguration
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule(new OrderItemManagerModule());
+
+            // Register the application class
+            builder.RegisterType<Application>();
+
+            var assembly = Assembly.GetExecutingAssembly();
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces();
+
+            // Set the dependency resolver to be Autofac.
+            return builder.Build();
+
+
+        }
+    }
+}
