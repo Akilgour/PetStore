faeeea77d0ceb3eb10ce4d42e31c606430ccf51e
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-19 16:53:00 +0100

    Added empty StockDeliveryController and stubbed out Post

---
+++ PertStore.API/Controllers/StockDeliveryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetStore.Shared.DTO;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PetStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class StockDeliveryController : Controller
+    {
+        [HttpPost]
+        public async Task<ActionResult> Post(StockDeliveryCreate stockDeliveryCreate)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ PetStore.Shared/DTO/StockDeliveryCreate.cs
@@ -0,0 +1,8 @@
+﻿namespace PetStore.Shared.DTO
+{
+    public class StockDeliveryCreate
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
