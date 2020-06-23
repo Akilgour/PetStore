3452d82aa467c66286647e76d1c38a7117f91b7f
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-23 12:17:08 +0100

    Added the controllers

---
+++ PetStore.Blazor.WASM/Server/Controllers/StockDeliveryController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetStore.Blazor.WASM.Server.Manager.Interface;
+using PetStore.Shared.DTO;
+using System.Threading.Tasks;
+
+namespace PetStore.Blazor.WASM.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockDeliveryController : ControllerBase
+    {
+        private readonly IStockDeliveryManager _stockDeliveryManager;
+
+        public StockDeliveryController(IStockDeliveryManager stockDeliveryManager)
+        {
+            _stockDeliveryManager = stockDeliveryManager ?? throw new System.ArgumentNullException(nameof(stockDeliveryManager));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(StockDeliveryCreate stockDeliveryCreate)
+        {
+            await _stockDeliveryManager.AddStock(stockDeliveryCreate);
+            return Ok();
+        }
+    }
+}
