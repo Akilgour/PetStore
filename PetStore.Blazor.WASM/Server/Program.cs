2a1e2aee54c3f7d52a1d33b8508e122d8c87aaa1
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-23 18:50:24 +0100

    Set up swagger

--- PetStore.Blazor.WASM/Server/Program.cs
+++ PetStore.Blazor.WASM/Server/Program.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace PetStore.Blazor.WASM.Server
 {
@@ -13,14 +9,14 @@ namespace PetStore.Blazor.WASM.Server
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                 .ConfigureServices(services => services.AddAutofac());
+        }
     }
 }
--- PetStore.Blazor.WASM/Server/Startup.cs
+++ PetStore.Blazor.WASM/Server/Startup.cs
@@ -6,6 +6,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Linq;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System.IO;
+using System;
+using Autofac;
+using AutoMapper.Contrib.Autofac.DependencyInjection;
 
 namespace PetStore.Blazor.WASM.Server
 {
@@ -25,6 +31,30 @@ namespace PetStore.Blazor.WASM.Server
 
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            //Added Swagger
+            services.AddSwaggerGen(setUpAction =>
+            {
+                setUpAction.SwaggerDoc("PetStoreAPI", new OpenApiInfo { Title = "PetStore API", Version = "1" });
+
+                //Add comments, to get this to work you need to go into project properties, build tab, then select "XML Documentation file"
+                var xmlCommentFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlCommentFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentFile);
+                setUpAction.IncludeXmlComments(xmlCommentFullPath);
+            });
+        }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            // Add any Autofac modules or registrations.
+            // This is called AFTER ConfigureServices so things you
+            // register here OVERRIDE things registered in ConfigureServices.
+            //
+            // You must have the call to `UseServiceProviderFactory(new AutofacServiceProviderFactory())`
+            // when building the host or this won't be called.
+
+            builder.RegisterModule(new Autofac.AutofacConfiguration());
+            builder.AddAutoMapper(typeof(Startup).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +73,14 @@ namespace PetStore.Blazor.WASM.Server
             }
 
             app.UseHttpsRedirection();
+
+            //Added Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(setupAction =>
+            {
+                setupAction.SwaggerEndpoint("/swagger/PetStoreAPI/swagger.json", "PetStore API");
+            });
+
             app.UseBlazorFrameworkFiles();
             app.UseStaticFiles();
 
