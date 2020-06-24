e4f64276c323e2b3190a00bdf4be6567b715e9ed
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-24 16:41:24 +0100

    Added OrderItemsNewTableRow

---
+++ PetStore.Blazor.WASM/Client/Components/OrderItemsNewTableRowBase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using PetStore.Blazor.WASM.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore.Blazor.WASM.Client.Components
+{
+    public class OrderItemsNewTableRowBase : ComponentBase
+    {
+        [Parameter]
+        public  OrderItemsNew OrderItem { get; set; }
+    }
+}
--- PetStore.Blazor.WASM/Shared/Models/StockOrderNew.cs
+++ PetStore.Blazor.WASM/Shared/Models/StockOrderNew.cs
@@ -5,6 +5,6 @@ namespace PetStore.Blazor.WASM.Shared.Models
     public class StockOrderNew
     {
         public string OrderNumber { get; set; }
-        public List<OrderItemsNew> OrderItems { get; set; }
+        public List<OrderItemsNew> OrderItems { get; set; } = new List<OrderItemsNew>();
     }
 }
