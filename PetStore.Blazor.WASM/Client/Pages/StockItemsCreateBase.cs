6414549dbea3e4b80637f650c3e3cd16312f036f
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-24 10:27:55 +0100

    Added empty stock item page

---
+++ PetStore.Blazor.WASM/Client/Pages/StockItemsCreateBase.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace PetStore.Blazor.WASM.Client.Pages
+{
+    public class StockItemsCreateBase : ComponentBase
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        //used to store state of screen
+        protected string Message = string.Empty;
+        protected string StatusClass = string.Empty;
+        protected bool Saved;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await Task.Delay(2000);
+        }
+
+        protected async Task HandleValidSubmit()
+        {
+            await Task.Delay(2000);
+            StatusClass = "alert-success";
+            Message = "Comment successfully.";
+            Saved = true;
+        }
+
+        protected void HandleInvalidSubmit()
+        {
+            StatusClass = "alert-danger";
+            Message = "There are some validation errors. Please try again.";
+        }
+
+        protected void NavigateBack()
+        {
+            NavigationManager.NavigateTo("");
+        }
+    }
+}
---
+++ PetStore.Blazor.WASM/Shared/Models/StockItemsNew.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetStore.Blazor.WASM.Server.Models
+{
+    public class StockItemsNew
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Quantity must be between 0 and 100.")]
+        public int Quantity { get; set; }
+    }
+}
