dd2ac0334d51cbcc7e529eafbc79b09c249411e8
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-29 17:38:18 +0100

    Unpicked the base classes as the call back wasnt working when when called for a second time. It was becasue the client class was not inheriting IDisposable Doing a check in while it works and then going to clean up

--- PetStore.OrderItem.Client/Client/OrderItemClient.cs
+++ PetStore.OrderItem.Client/Client/OrderItemClient.cs
@@ -2,8 +2,7 @@
 using PetStore.OrderItem.Client.Client.Interface;
 using PetStore.Shared.Helpers;
 using PetStore.Shared.QueMessages;
-using PetStore.Shared.RabbitMQ;
-using PetStore.Shared.RabbitMQ.Factorys;
+using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System;
 using System.Collections.Concurrent;
@@ -11,21 +10,37 @@ using System.Threading.Tasks;
 
 namespace PetStore.OrderItem.Client
 {
-    public class OrderItemClient : BaseSendReceiveClient, IOrderItemClient
+    public class OrderItemClient : IDisposable, IOrderItemClient
     {
-        private readonly ConcurrentDictionary<string, TaskCompletionSource<OrderResponse>> _pendingMessages;
+    //    https://gigi.nullneuron.net/gigilabs/tag/taskcompletionsource/
+
+
+       private  ConcurrentDictionary<string, TaskCompletionSource<OrderResponse>> _pendingMessages;
         private const string _requestQueueName = "OrderItem_RequestQueue";
         private const string _responseQueueName = "OrderItem_ResponseQueue";
         private const string _exchangeName = ""; // default exchange
+        private EventingBasicConsumer _consumer;
+        private IConnection _connection;
+        private IModel _channel;
+
+        private bool disposed = false;
 
         public OrderItemClient()
-            : base(RabbitMQConfigFactory.Create(), _requestQueueName, _responseQueueName, _exchangeName)
         {
-            _pendingMessages = new ConcurrentDictionary<string, TaskCompletionSource<OrderResponse>>();
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(_requestQueueName, true, false, false, null);
+            _channel.QueueDeclare(_responseQueueName, true, false, false, null);
+            _consumer = new EventingBasicConsumer(this._channel);
+            _consumer.Received += Receive;
+            _channel.BasicConsume(_responseQueueName, true, _consumer);
+            var replyQueueName = _channel.QueueDeclare().QueueName;
         }
 
         public Task<OrderResponse> Send(StockOrder stockOrder)
         {
+            _pendingMessages = new ConcurrentDictionary<string, TaskCompletionSource<OrderResponse>>();
             var message = stockOrder.Serialize();
             var tcs = new TaskCompletionSource<OrderResponse>();
             var correlationId = Guid.NewGuid().ToString();
@@ -34,15 +49,46 @@ namespace PetStore.OrderItem.Client
             return tcs.Task;
         }
 
-        protected override void Receive(object sender, BasicDeliverEventArgs e)
+        private void Publish(byte[] message, string correlationId)
+        {
+            var props = this._channel.CreateBasicProperties();
+            props.CorrelationId = correlationId;
+            props.ReplyTo = _responseQueueName;
+            this._channel.BasicPublish(_exchangeName, _requestQueueName, props, message);
+        }
+
+        protected void Receive(object sender, BasicDeliverEventArgs e)
         {
             var correlationId = e.BasicProperties.CorrelationId;
             var orderResponse = (OrderResponse)e.Body.ToArray().DeSerialize(typeof(OrderResponse));
+
+            //On the first time round _pendingMessages has one item in it, and is the item we are looking for
+            //On the second call it is empty
             this._pendingMessages.TryRemove(correlationId, out var tcs);
             if (tcs != null)
             {
                 tcs.SetResult(orderResponse);
             }
         }
+
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed && disposing)
+            {
+                _channel?.Dispose();
+                _connection?.Dispose();
+            }
+
+            this.disposed = true;
+        }
     }
+
 }
--- PetStore.OrderItem.Server/Application.cs
+++ PetStore.OrderItem.Server/Application.cs
@@ -1,40 +1,81 @@
 ﻿using PetStore.Domain.Models;
 using PetStore.OrderItem.Manager.Manager.Interface;
 using PetStore.Shared.Helpers;
+using PetStore.Shared.QueMessages;
 using PetStore.Shared.RabbitMQ;
 using PetStore.Shared.RabbitMQ.Factorys;
+using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System;
 using System.Threading.Tasks;
 
 namespace PetStore.OrderItem.Server
 {
-    public class Application : BaseReceiveReplyServer
+    public class Application // : BaseReceiveReplyServer
     {
         private const string _requestQueueName = "OrderItem_RequestQueue";
         private readonly IOrderItemManager _orderItemManager;
         private const string _exchangeName = "";
+        private static IModel channel;
 
         public Application(IOrderItemManager orderItemManager)
-           : base(RabbitMQConfigFactory.Create(), _requestQueueName, _exchangeName)
+           //: base(RabbitMQConfigFactory.Create(), _requestQueueName, _exchangeName)
         {
+          
             _orderItemManager = orderItemManager;
         }
 
         public void Run()
         {
-            WaitForResponse();
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (channel = connection.CreateModel())
+                {
+                    const string requestQueueName = _requestQueueName;
+                    channel.QueueDeclare(requestQueueName,  true, false, false, null);
+ 
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += Receive;
+                    channel.BasicConsume(requestQueueName, true, consumer);
+
+                    Console.WriteLine("Waiting for messages...");
+                    Console.WriteLine("Press ENTER to exit.");
+                    Console.WriteLine();
+                    Console.ReadLine();
+                }
+            }
         }
 
-        protected override async Task Receive(object sender, BasicDeliverEventArgs e)
+        protected void Receive(object sender, BasicDeliverEventArgs e)
         {
             var stockOrder = (StockOrder)e.Body.ToArray().DeSerialize(typeof(StockOrder));
             var correlationId = e.BasicProperties.CorrelationId;
             string responseQueueName = e.BasicProperties.ReplyTo;
 
             Console.WriteLine($"Received: {stockOrder.OrderNumber} with CorrelationId {correlationId}");
-            var responseMessage = await _orderItemManager.Order(stockOrder);
-            Reply( correlationId, responseQueueName, responseMessage.Serialize());
+            // var responseMessage = foo(stockOrder);
+            var responseMessage = foo(stockOrder);
+            Publish( correlationId, responseQueueName, responseMessage.Serialize());
+        }
+
+        private OrderResponse foo(StockOrder stockOrder)
+        {
+            return new OrderResponse() { Success = true, Message = "bar" };
+        }
+     
+
+        private static void Publish(  string correlationId, string responseQueueName, byte[] item)
+        {
+            const string exchangeName = ""; // default exchange
+            var responseProps = channel.CreateBasicProperties();
+            responseProps.CorrelationId = correlationId;
+
+            channel.BasicPublish(exchangeName, responseQueueName, responseProps, item);
+
+            
         }
     }
 }
