ef3034a9eebc00e5e4cda5fc9aeac47d3c33ea94
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-30 11:05:50 +0100

    Added dispaly message to stock delivery Made it be a random colour to make it look nicer

--- PetStore.Shared/Helpers/ScopedConsoleColourHelper.cs
+++ PetStore.Shared/Helpers/ScopedConsoleColourHelper.cs
@@ -8,8 +8,46 @@ namespace PetStore.Shared.Helpers
 
         public ScopedConsoleColourHelper(ConsoleColor newColour)
         {
+            SetColour(newColour);
+        }
+
+        private void SetColour(ConsoleColor newColour)
+        {
             _oldColour = Console.ForegroundColor;
             Console.ForegroundColor = newColour;
+        }
+
+        public ScopedConsoleColourHelper()
+        {
+            var rand = new Random();
+            int randomNumber = rand.Next(1, 6);
+
+            switch (randomNumber)
+            {
+                case 1:
+                    SetColour(ConsoleColor.Blue);
+                    break;
+
+                case 2:
+                    SetColour(ConsoleColor.Green);
+                    break;
+
+                case 3:
+                    SetColour(ConsoleColor.Cyan);
+                    break;
+
+                case 4:
+                    SetColour(ConsoleColor.Red);
+                    break;
+
+                case 5:
+                    SetColour(ConsoleColor.Magenta);
+                    break;
+
+                default:
+                    SetColour(ConsoleColor.White);
+                    break;
+            }
         }
 
         public void Dispose()
--- PetStore.StockDelivery/Application.cs
+++ PetStore.StockDelivery/Application.cs
@@ -4,6 +4,7 @@ using PetStore.Shared.RabbitMQ;
 using PetStore.Shared.RabbitMQ.Factorys;
 using PetStore.StockDelivery.Manager.Managers.Interface;
 using RabbitMQ.Client.Events;
+using System;
 using System.Threading.Tasks;
 
 namespace PetStore.StockDelivery
@@ -26,6 +27,12 @@ namespace PetStore.StockDelivery
         protected override async Task Received(object sender, BasicDeliverEventArgs @event)
         {
             var stockItem = (StockItem)@event.Body.ToArray().DeSerialize(typeof(StockItem));
+
+            using (var colour = new ScopedConsoleColourHelper())
+            {
+                Console.WriteLine($"Name: {stockItem.Name} Quantity: {stockItem.Quantity}");
+            }
+
             await _stockDeliveryManager.Create(stockItem);
         }
     }
