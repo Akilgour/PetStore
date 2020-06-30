db81e75baaed3bb7fc2c74c93bc8e4cdd1a93b51
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-30 11:41:18 +0100

    Stubbed out add row not sure if i wil move to a componet

--- PetStore.Blazor.WASM/Client/Components/StockDeliveryNewTableBase.cs
+++ PetStore.Blazor.WASM/Client/Components/StockDeliveryNewTableBase.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using PetStore.Blazor.WASM.Shared.Models;
 using System.Collections.Generic;
 
@@ -8,5 +9,21 @@ namespace PetStore.Blazor.WASM.Client.Components
     {
         [Parameter]
         public List<StockDeliveryCreate> StockDeliveryList { get; set; }
+        protected bool ShowAdd { get; private set; }
+
+        protected override void OnInitialized()
+        {
+            ShowAdd = false;
+        }
+
+        public void ShowAddItem(MouseEventArgs e)
+        {
+            ShowAdd = true;
+        }
+
+        public void HideAddItem(MouseEventArgs e)
+        {
+            ShowAdd = false;
+        }
     }
 }
