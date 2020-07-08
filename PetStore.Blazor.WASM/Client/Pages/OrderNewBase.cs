3ab8a5a0e05e565954d7bd1b8ce8dd689a41c4de
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-07-08 11:58:58 +0100

    Added stub page for ordering

---
+++ PetStore.Blazor.WASM/Client/Pages/OrderNewBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetStore.Blazor.WASM.Client.Pages
+{
+    public class OrderNewBase : ComponentBase
+    {
+    }
+}
