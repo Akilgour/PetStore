20bb64c0b50c88f5445cf4ac1e70034854f6ecbc
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-07-08 13:02:25 +0100

    Adde display of stock tiles

--- PetStore.Blazor.WASM/Client/Pages/OrderNewBase.cs
+++ PetStore.Blazor.WASM/Client/Pages/OrderNewBase.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Components;
-using System;
+using PetStore.Blazor.WASM.Shared.Models;
 using System.Collections.Generic;
-using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace PetStore.Blazor.WASM.Client.Pages
 {
     public class OrderNewBase : ComponentBase
     {
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        public List<StockItemDisplay> StockItems { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            StockItems = await Http.GetJsonAsync<List<StockItemDisplay>>("api/Stock");
+        }
     }
 }
