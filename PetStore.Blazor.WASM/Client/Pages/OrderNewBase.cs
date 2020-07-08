61346c4de240d6bd1503b6d7ffd0981fdd41dc2c
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-07-08 14:41:09 +0100

    Added Blazored.Typeahead this give a auto complete text box

--- PetStore.Blazor.WASM/Client/Pages/OrderNewBase.cs
+++ PetStore.Blazor.WASM/Client/Pages/OrderNewBase.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
 using PetStore.Blazor.WASM.Shared.Models;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -10,12 +11,18 @@ namespace PetStore.Blazor.WASM.Client.Pages
     {
         [Inject]
         public HttpClient Http { get; set; }
+        protected StockItemDisplay SelectedStock;
 
         public List<StockItemDisplay> StockItems { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
             StockItems = await Http.GetJsonAsync<List<StockItemDisplay>>("api/Stock");
+        }
+
+        protected async Task<IEnumerable<StockItemDisplay>> SearchStock(string searchText)
+        {
+            return await Task.FromResult(StockItems.Where(x => x.Name.ToLower().Contains(searchText.ToLower())).ToList());
         }
     }
 }
