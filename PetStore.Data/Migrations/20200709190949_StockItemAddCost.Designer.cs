d38c41178511142e31615147a3902c03f58e45ca
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-07-09 20:16:40 +0100

    Adds CostInPounds to the stockItem table

--- PetStore.Data/Migrations/20200709190949_StockItemAddCost.Designer.cs
+++ PetStore.Data/Migrations/20200709190949_StockItemAddCost.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PetStore.Data.Context;
 
 namespace PetStore.Data.Migrations
 {
     [DbContext(typeof(PetStoreContext))]
-    partial class PetStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20200709190949_StockItemAddCost")]
+    partial class StockItemAddCost
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
