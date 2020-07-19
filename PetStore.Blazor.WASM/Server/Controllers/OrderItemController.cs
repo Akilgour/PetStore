92d0eac7dc22b46c933fa0af59f34f4754863ffd
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-07-19 22:39:50 +0100

    Added ApiEndpoints package Scrapped OrderItemController and swapped it over for a end point

--- PetStore.Blazor.WASM/Server/Controllers/OrderItemController.cs
+++ PetStore.Blazor.WASM/Server/Controllers/OrderItemController.cs
@@ -1,34 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using PetStore.Blazor.WASM.Server.Manager.Interface;
-using PetStore.Blazor.WASM.Shared.Models;
-using System.Threading.Tasks;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using PetStore.Blazor.WASM.Server.Manager.Interface;
+//using PetStore.Blazor.WASM.Shared.Models;
+//using System.Threading.Tasks;
 
-namespace PetStore.Blazor.WASM.Server.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class OrderItemController : ControllerBase
-    {
-        private readonly IOrderItemManager _orderItemManager;
+//namespace PetStore.Blazor.WASM.Server.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class OrderItemController : ControllerBase
+//    {
+//        private readonly IOrderItemManager _orderItemManager;
 
-        public OrderItemController(IOrderItemManager orderItemManager)
-        {
-            _orderItemManager = orderItemManager ?? throw new System.ArgumentNullException(nameof(orderItemManager));
-        }
+//        public OrderItemController(IOrderItemManager orderItemManager)
+//        {
+//            _orderItemManager = orderItemManager ?? throw new System.ArgumentNullException(nameof(orderItemManager));
+//        }
 
-        // POST api/<controller>
-        [HttpPost]
-        public async Task<ActionResult> Post(StockOrderCreate stockOrderCreate)
-        {
-            var result = await _orderItemManager.OrderCreate(stockOrderCreate);
-            if (result.Success)
-            {
-                return Ok(result);
-            }
-            else
-            {
-                return BadRequest(result);
-            }
-        }
-    }
-}
+//        // POST api/<controller>
+//        [HttpPost]
+//        public async Task<ActionResult> Post(StockOrderCreate stockOrderCreate)
+//        {
+//            var result = await _orderItemManager.OrderCreate(stockOrderCreate);
+//            if (result.Success)
+//            {
+//                return Ok(result);
+//            }
+//            else
+//            {
+//                return BadRequest(result);
+//            }
+//        }
+//    }
+//}
